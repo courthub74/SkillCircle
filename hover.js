f66c14d69f0d1ec0f181f0b46bd23fe595016de4
Courdevelops <courdevelops@gmail.com>
2024-05-23 18:18:27 -0400

    skills circles work

--- hover.js
+++ hover.js
@@ -10,8 +10,12 @@ let line_two = document.getElementById('line_two');
 let line_three = document.getElementById('line_three');
 
 let python_logo = document.getElementById('python');
+let python_logo_two = document.getElementById('python_two');
+let python_logo_three = document.getElementById('python_three');
 
 let python_description = document.getElementById('python_description');
+let python_description_two = document.getElementById('python_description_two');
+let python_description_three = document.getElementById('python_description_three');
 
 // For Mobiles
 
@@ -64,33 +68,45 @@ if(widthMatch.matches) {
     circle_one.addEventListener('mouseover', () => {
         circle_one.classList.add('hover');
         line_one.classList.add('hover');
+        python_logo.classList.add('hover');
+        python_description.classList.add('hover');
     });
     
     circle_one.addEventListener('mouseout', () => {
         circle_one.classList.remove('hover');
         line_one.classList.remove('hover');
+        python_logo.classList.remove('hover');
+        python_description.classList.remove('hover');
     });
     
     
     circle_two.addEventListener('mouseover', () => {
         circle_two.classList.add('hover');
         line_two.classList.add('hover');
+        python_logo_two.classList.add('hover');
+        python_description_two.classList.add('hover');
     });
     
     circle_two.addEventListener('mouseout', () => {
         circle_two.classList.remove('hover');
         line_two.classList.remove('hover');
+        python_logo_two.classList.remove('hover');
+        python_description_two.classList.remove('hover');
     });
     
     
     circle_three.addEventListener('mouseover', () => {
         circle_three.classList.add('hover');
         line_three.classList.add('hover');
+        python_logo_three.classList.add('hover');
+        python_description_three.classList.add('hover');
     });
     
     circle_three.addEventListener('mouseout', () => {
         circle_three.classList.remove('hover');
         line_three.classList.remove('hover');
+        python_logo_three.classList.remove('hover');
+        python_description_three.classList.remove('hover');
     });
 }
 
